2efe2c1f256e3ad9f4ad4e6f131a6db76d4c9e11
05972Kas <71328873+05972Kas@users.noreply.github.com>
2020-09-30 14:22:35 -0400

    Update A1.java
    Added Comments

--- A1.java
+++ A1.java
@@ -1,5 +1,16 @@
+/* Name: Christopher Kastelec
+* Course: ICS 3U
+* Teacher Mrs. McCaffery
+*Description:
+*
+*
+*/
+
+
 class Main {
+  
   public static void main(String[] args) {
-    System.out.println("Hi how are you?");
+    System.out.println("Hi,\nhow are you?");
+    //prints out Hi, how are you?
   }
 }
