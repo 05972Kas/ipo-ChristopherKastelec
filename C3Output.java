1a1372d23678a1d0d65384f78c24601d4a6c386e
05972Kas <71328873+05972Kas@users.noreply.github.com>
2020-09-30 14:23:56 -0400

    Create C3Output.java
    A program that prints out a Diamond, Triangle and Square.

---
+++ C3Output.java
@@ -0,0 +1,28 @@
+/* Name: Christopher Kastelec
+* Course: ICS 3U
+* Teacher Mrs. McCaffery
+*Description:
+*
+*
+*/
+
+//package outputsolution;
+
+public class Main {
+
+  static String taskValue = "B2";
+
+          public static void main(String[] args) {
+                //this prints out the triangle
+                System.out.println("      D\n     I I\n    A   A\n   M     M\n    O   O\n     N N\n      D\n\n");
+                //this prints out the triangle
+                System.out.println("       T\n      R R\n     I   I\n    A     A\n   N       N\n  G         G\n LlLlLlLlLlLlL\n\n");
+                //this prints out a rectangle
+                System.out.println(" RRRRR\n E   E\n C   C\n T   T\n A   A\n N   N\n G   G\n L   L\n EEEEE\n\n");
+              
+              
+              //returns all shapes into the console
+            }  //end main
+
+
+} //end Main
